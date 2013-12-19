506841853173c32f24af2deaf531db54449c3b71
unknown <ddvergsten@Butternut.satsys.local>
2013-12-19 12:53:19 -0600

    add test circuit

--- rotateLR/barrelShifter.vhd
+++ rotateLR/barrelShifter.vhd
@@ -31,6 +31,7 @@ use IEEE.STD_LOGIC_1164.ALL;
 
 entity barrel_shifter is
     Port ( a : in  STD_LOGIC_VECTOR(7 downto 0);
+				--dir: in STD_LOGIC;
            amt : in  STD_LOGIC_VECTOR(2 downto 0);
            y : out  STD_LOGIC_VECTOR(7 downto 0)
 			  );
@@ -53,12 +54,23 @@ end sel_arch;
 architecture multi_stage_arch of barrel_shifter is
 	signal s0, s1: std_logic_vector(7 downto 0);
 begin
-	--stage 0, shift 0 or 1 bit
 	s0 <= a(0) & a(7 downto 1) when amt(0)='1' else
 			a;
 	s1 <= s0(1 downto 0) & s0(7 downto 2) when amt(1) = '1' else
 			s0;
 	y <= s1(3 downto 0) & s1(7 downto 4) when amt(2) = '1' else
 			s1;
+--process(s0, s1, s2, dir, a, amt)
+--begin
+
+--if dir = '1' then
+	--y <= s2;
+	--stage 0, shift 0 or 1 bit
+	
+			--else
+			--y <= s2;
+	--stage 0,
+			--end if;
+	--end process;
 end multi_stage_arch;
 
